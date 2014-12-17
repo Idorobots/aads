a379bccd46f8c2b512095b971d347e62fdea8da7
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2014-12-17 22:11:48 +0100

    Fixed array aliasing bug in readChunks.

--- huffman/src/utils.d
+++ huffman/src/utils.d
@@ -4,7 +4,13 @@ import std.stdio;
 import std.array;
 
 ubyte[][] readChunks(File f, size_t chunksize) {
-    return f.byChunk(chunksize).array();
+    auto arr = appender!(ubyte[][])();
+
+    foreach(ubyte[] chunk; f.byChunk(chunksize)) {
+        arr ~= chunk.dup;
+    }
+
+    return arr.data;
 }
 
 size_t currPos(File f, size_t pos = 0) {
