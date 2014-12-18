caf744628d7e84176e642928fc51ad27685aa907
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2014-12-18 23:18:39 +0100

    Cleaned up the repo a bit.

---
+++ huffman/test/test.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+for CHUNK in `seq -w 1 100`; do
+    more seneca.txt | ./huffman -s$CHUNK -c -o "seneca.huf$CHUNK"
+done;
+
+# du -b seneca.huf* | sed s/seneca.huf// | awk '{ print $2 "\t" $1}' >> test.out
