afb1d6a2302ccc60fc9c47d8c8a68b43d0033ee6
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2014-12-19 23:24:52 +0100

    Fixed collinear points bug.
    Still needs a way to filter out all the collinear points.

--- graham/src/graham.d
+++ graham/src/graham.d
@@ -6,11 +6,15 @@ import std.math;
 import std.conv;
 
 struct Point {
-    float x;
-    float y;
+    double x;
+    double y;
 
-    @property float angle(Point other) {
+    @property double angle(Point other) {
         return atan2(other.y - y, other.x - x);
+    }
+
+    @property double distance (Point other) {
+        return sqrt((other.x - x)^^2 + (other.y - y)^^2);
     }
 
     string toString() {
@@ -18,7 +22,7 @@ struct Point {
     }
 }
 
-float ccw(Point p1, Point p2, Point p3) {
+double ccw(Point p1, Point p2, Point p3) {
     return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
 }
 
@@ -31,24 +35,28 @@ void swap(ref Point[] points, uint i, uint j) {
 Point[] convexHull(Point[] points) {
     auto N = points.length;
 
-    points.sort!((p1, p2) => p1.y < p2.y)();
-    points.sort!((p1, p2) => p1.x < p1.x)();
+    points.sort!((p1, p2) => (p1.y != p2.y) ? p1.y < p2.y : p1.x < p2.x)();
 
     auto p0 = points[0];
 
-    points[1..$].sort!((p1, p2) => p0.angle(p1) < p0.angle(p2));
+    points.sort!((p1, p2) {
+            auto a1 = p0.angle(p1);
+            auto a2 = p0.angle(p2);
+            return (a1 != a2) ? a1 < a2 : p0.distance(p2) >= p0.distance(p1);
+        });
+
+    // FIXME Filter out the collinear points.
 
     auto M = 1;
 
     for(uint i = 2; i < N; ++i) {
-        while(ccw(points[M-1], points[M], points[i]) <= 0) {
+        while(ccw(points[M-1], points[M], points[i]) < 0) {
             if(M > 1) --M;
             else if(i == N-1) break;
             else ++i;
         }
 
-        ++M;
-        points.swap(i, M);
+        points.swap(i, ++M);
     }
 
     return points[0..M+1];
