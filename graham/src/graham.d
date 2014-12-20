cd44cecd60f3862f7f41a130654d11f41c92173f
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2014-12-20 23:03:28 +0100

    Added handling of collinear points in Graham scan.
    Also, using a stack instead of double indexed array for fun and profit.

--- graham/src/graham.d
+++ graham/src/graham.d
@@ -2,6 +2,7 @@ module graham;
 
 import std.typecons;
 import std.algorithm;
+import std.array;
 import std.math;
 import std.conv;
 
@@ -26,38 +27,45 @@ double ccw(Point p1, Point p2, Point p3) {
     return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
 }
 
-void swap(ref Point[] points, uint i, uint j) {
-    auto tmp = points[i];
-    points[i] = points[j];
-    points[j] = tmp;
-}
-
 Point[] convexHull(Point[] points) {
-    auto N = points.length;
-
+    // Sort points by coords.
     points.sort!((p1, p2) => (p1.y != p2.y) ? p1.y < p2.y : p1.x < p2.x)();
 
     auto p0 = points[0];
 
+    // Sort points by polar coords to p0.
     points.sort!((p1, p2) {
             auto a1 = p0.angle(p1);
             auto a2 = p0.angle(p2);
             return (a1 != a2) ? a1 < a2 : p0.distance(p2) >= p0.distance(p1);
         });
 
-    // FIXME Filter out the collinear points.
+    // Remove duplicates for good measure.
+    points = points.uniq().array();
 
-    auto M = 1;
+    auto N = points.length;
 
-    for(uint i = 2; i < N; ++i) {
-        while(ccw(points[M-1], points[M], points[i]) < 0) {
-            if(M > 1) --M;
-            else if(i == N-1) break;
-            else ++i;
-        }
-
-        points.swap(i, ++M);
+    // Skip colinear points.
+    uint i;
+    for(i = 2; i < N; ++i) {
+        if(ccw(p0, points[1], points[i]) != 0) break;
     }
 
-    return points[0..M+1];
+    auto hull = [p0, points[i - 1]];
+
+    // ???
+    for(uint j = i; j < N; ++j) {
+        Point top;
+
+        do {
+            top = hull[$-1];
+            hull = hull[0..$-1];
+        } while(ccw(hull[$-1], top, points[j]) <= 0);
+
+        hull ~= top;
+        hull ~= points[j];
+    }
+
+    // PROFIT!
+    return hull;
 }
