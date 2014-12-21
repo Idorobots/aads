4cea9ca5b47680a94307f171bcedf8bf6281f9f5
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2014-12-21 21:58:45 +0100

    Extended file IO support.
    Also, added usage info.

--- graham/src/main.d
+++ graham/src/main.d
@@ -1,3 +1,4 @@
+import std.c.stdlib;
 import std.stdio;
 import std.getopt;
 import std.conv;
@@ -8,20 +9,54 @@ import graham;
 
 void main(string[] args) {
     bool completeHull = false;
+    string infile;
+    string outfile;
 
-    getopt(args,
-           "complete|c", &completeHull);
-    Point[] points;
-
-    foreach(line; stdin.byLine()) {
-        auto a = line.split(", ").map!(to!double);
-        points ~= Point(a[0], a[1]);
+    void help() {
+        writeln("Usage: ", args[0], " [OPTIONS]");
+        writeln();
+        writeln("OPTIONS:");
+        writefln("  %-20s%s", "-h --help", "Display this message.");
+        writefln("  %-20s%s", "-i --infile", "Input file name.");
+        writefln("  %-20s%s", "-o --outfile", "Output file name.");
+        writefln("  %-20s%s", "-c --complete", "Specifies whether convex hull shuld begin and end at the same point.");
+        exit(0);
     }
 
-    auto hull = convexHull(points);
-    foreach(p; hull) {
-        writeln(p.x, ", ", p.y);
-    }
+    try {
+        getopt(args,
+               "infile|i", &infile,
+               "outfile|o", &outfile,
+               "complete|c", &completeHull,
+               "help|h", &help);
 
-    if(completeHull) writeln(hull[0].x, ", ", hull[0].y);
+        auto input = stdin;
+        auto output = stdout;
+
+        if(infile != "") {
+            input = File(infile, "rb");
+        }
+
+        if(outfile != "") {
+            output = File(outfile, "wb");
+        }
+
+        Point[] points;
+
+        foreach(line; input.byLine()) {
+            auto a = line.split(", ").map!(to!double);
+            points ~= Point(a[0], a[1]);
+        }
+
+        auto hull = convexHull(points);
+        foreach(p; hull) {
+            output.writeln(p.x, ", ", p.y);
+        }
+
+        if(completeHull) output.writeln(hull[0].x, ", ", hull[0].y);
+
+    } catch(Exception e) {
+        writeln("Error: ", e.msg);
+        help();
+    }
 }
