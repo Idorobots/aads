8ed7ff48067a8450865c69effb749a230e702a49
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-01-20 20:19:03 +0100

    Added Rabin-Karp implementation.

--- string/src/rk.d
+++ string/src/rk.d
@@ -1,5 +1,39 @@
 module rk;
 
+import std.algorithm;
+
+size_t hash(string str) {
+    return str.reduce!((h, c) => (h << 1) + c)();
+}
+
+size_t rehash(char first, char last, size_t hash, size_t discriminant) {
+    return ((cast(long) hash - first * cast(long) discriminant) << 1) + last;
+}
+
 size_t[] rabinKarp(string pattern, string text) {
-    return [];
+    if(text.length == 0 || pattern.length == 0) return [];
+    if(text.length < pattern.length) return [];
+
+    size_t[] indeces;
+
+    size_t d = 1 << (pattern.length - 1);
+    auto pHash = hash(pattern);
+    auto tHash = hash(text[0 .. pattern.length]);
+
+    size_t i = 0;
+
+    for(;;) {
+        if(pHash == tHash && pattern == text[i .. pattern.length + i]) {
+            indeces ~= i;
+        }
+
+        if(i < text.length - pattern.length) {
+            tHash = rehash(text[i], text[i + pattern.length], tHash, d);
+        } else {
+            break;
+        }
+        ++i;
+    }
+
+    return indeces;
 }
